b3a753ca3f7aa549b981cefa6ebba6ec14110f57
Shweta Singh <shwetas6913@gmail.com>
2026-04-14 23:47:42 +0530

    Learning Reactjs

---
+++ demo/src/App.jsx
@@ -0,0 +1,24 @@
+import Page from './page1.jsx'
+function App()
+{
+  let name='shweta'
+  return(
+    // <h1>Hello This is App Page</h1>
+    // <h1>Hello This is App Page</h1> ---error ayegi aise me
+    <>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+        <h1>Hello This is App Page</h1>
+       <Page/>   
+       {name} 
+    </>
+  )
+}
+export default App
---
+++ demo/src/page1.jsx
@@ -0,0 +1,7 @@
+function Page()
+{
+    return(
+        <h1>This is section page</h1>
+    )
+}
+export default Page
