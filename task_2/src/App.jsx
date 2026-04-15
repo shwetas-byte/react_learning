9ea7fd20874822efe41ff6f7aeb04228a29fd561
Shweta Singh <shwetas6913@gmail.com>
2026-04-15 14:52:45 +0530

    Learning React

---
+++ task_2/src/App.jsx
@@ -0,0 +1,20 @@
+import Home from './home.jsx'
+import About from './about.jsx'
+import Contact from './contact.jsx'
+import Login from './login.jsx'
+import Learn from './learn.jsx'
+function App()
+{
+  return(
+    <>
+    <h1>Learning React Js and doing task</h1>
+    <Home />
+    <About />
+    <Contact />
+    <Login />
+    <Learn />
+    </>
+
+  )
+}
+export default App
---
+++ task_2/src/learn.jsx
@@ -0,0 +1,7 @@
+function Learn()
+{
+    return(
+        <h1>This is Learn page</h1>
+    )
+}
+export default Learn
