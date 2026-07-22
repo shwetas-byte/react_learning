897be065fb7df11d169b04d28795ac166c21e4cf
Shweta Singh <shwetas6913@gmail.com>
2026-07-22 11:50:18 +0530

    Learning react

--- learn_routing/src/Components/Login.jsx
+++ learn_routing/src/Components/Login.jsx
@@ -1,7 +1,20 @@
 export  function Login(){
     return(
         <>
-            <h1>Login Page</h1>
+            <section className="flex justify-center gap-[150px] h-[100vh] items-center bg-amber-200  ">
+                <div className="leading-[40px]"> 
+                    <h1 className=" font-semibold text-2xl ">Log in to your account</h1>
+                    <form action="">
+                        Email: <br />
+                        <input className="border border-gray-600 rounded-[5px]  pl-2 w-100 " type="text" placeholder="john@example.com"/> <br />
+                        Password: <br />
+                        <input className="border border-gray-600 rounded-[5px]  w-100 pl-2" type="password" placeholder="*******" />
+                    </form>
+                    <button className="bg-blue-800 mt-3 w-100 text-white rounded-[5px]">Submit</button>
+                    <p className="text-center">Don't have an account? <span className="text-blue-900 ">Sign up</span></p>
+                </div>
+                <div><img className="w-[500px] h-[500px] rounded-[5px]" src="img.jpg" alt="" /></div>
+            </section>
         </>
     )
 }
--- learn_routing/src/Components/Service.jsx
+++ learn_routing/src/Components/Service.jsx
@@ -1,7 +1,11 @@
 export  function Service(){
     return(
         <>
-            <h1>Service Page</h1>
+            <section className="bg-blue-700 p-4 m-2">
+                <h1 className="font-bold text-5xl text-center ">Service Page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, sapiente?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, dolore.</p>
+                <button className="border-2 round-[15px] ">Help</button>
+            </section>
         </>
     )
 }
